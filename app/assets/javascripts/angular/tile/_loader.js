c2f468258efa8c12babda806fe1a119524aaee13
Jason <jasontanner328@gmail.com>
2015-01-30 14:30:57 -0500

    created sample metro

--- app/assets/javascripts/angular/tile/_loader.js
+++ app/assets/javascripts/angular/tile/_loader.js
@@ -1,24 +1,2 @@
 angular.module('stats.metro_tile', [])
 
-.directive('metro-tile', metroTile);
-
-function metroTile() {
-  var directive = {
-    restrict: 'EA',
-    scope: {
-      'frontImg': '@',
-      'backImg': '@'
-    },
-    template: [
-      "<div class='live-tile'>",
-      "<img src='{{frontImg}}'>",
-      "<img src='{{backImg}}'>",
-      "</div>"
-    ].join(""),
-    link: function() {
-      angular.element('.live-tile').liveTile();
-    }
-  };
-
-  return directive;
-}
